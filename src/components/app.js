b880d79196e7106845c4c81326089721f01a490d
Jeff Brunson <jeff.a.brunson@gmail.com>
2016-08-16 21:48:53 -0600

    react-redux setup

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import { Component } from 'react';
+import React, { Component } from 'react';
+import BookList from '../containers/BookList';
+import BookDetail from '../containers/BookDetail';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <BookList />
+        <BookDetail />
+      </div>
     );
   }
 }
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Javascript', pages: 101 },
+    { title: 'Harry Potter', pages: 1000 },
+    { title: 'The Dark Tower', pages: 88 },
+    { title: 'Eloquent Ruby', pages: 10}
+  ]
+}
